cc1e96399d2dcf232d7957ecfe1dc071981e35d1
rzeZenphrix <emmanuel.okenwa08@gmail.com>
2025-02-22 22:11:21 +0300

    Update dependencies and fix vulnerabilities

--- config/database.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-}; 
